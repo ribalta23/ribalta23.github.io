db999f1f10d9048e7546f05d8f9bed9e04223318
ribalta23 <aleixribalta04@gmail.com>
2024-01-31 10:52:53 +0100

    Updated

---
+++ JavaScript/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const textElement = document.getElementById("text");
+    const textArray = ["Frontend Developer", "Programmer", "VideoGame Developer"];
+    let index = 0;
+    let isDeleting = false;
+    let typingSpeed = 250;
+
+    function type() {
+        const currentText = textArray[index];
+        if (!isDeleting) {
+            textElement.textContent = currentText.slice(0, textElement.textContent.length + 1);
+        } else {
+            textElement.textContent = currentText.slice(0, textElement.textContent.length - 1);
+        }
+
+        if (!isDeleting && textElement.textContent === currentText) {
+            isDeleting = true;
+            typingSpeed = 300;
+        } else if (isDeleting && textElement.textContent === "") {
+            isDeleting = false;
+            index = (index + 1) % textArray.length;
+            typingSpeed = 100;
+        }
+
+        setTimeout(type, typingSpeed);
+    }
+
+    type();
+});
+
+function copy() {
+    var correoBox = document.getElementById("correoBox");
+    var tempTextArea = document.createElement("textarea");
+    tempTextArea.value = correoBox.innerText;
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempTextArea);
+    Swal.fire({
+        title: 'Copied correctly!',
+        icon: 'success',
+        confirmButtonText: 'Close'
+    })
+}
---
+++ projects/Project2/Replica-of-a-website/README.md
@@ -0,0 +1,3 @@
+# Modul-M8-Replica-Vans
+
+Project to practice and guide me with a web already made. It's just an exercise I've done.
